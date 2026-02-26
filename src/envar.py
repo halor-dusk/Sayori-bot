3887f475709a91e37331ff8a8625cfe7d8e9c7fe
halor-dusk <sidneyenzorodriguesdasilva@gmail.com>
2026-02-26 09:26:17 +0000

    Adding the talking feature with personality and chatbot cohere ai and mention to username

--- src/envar.py
+++ src/envar.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 
-dotenc_path = find_dotenv()
-load_dotenv(dotenc_path)
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN: str = os.getenv("BOT_TOKEN")
+AI_KEY: str = os.getenv("AI_KEY")
--- src/main.py
+++ src/main.py
@@ -1,26 +1,88 @@
 import discord
+import cohere
+
 import envar
 
+import re
+
 def main() -> None:
-    print("Bot token is: ", envar.BOT_TOKEN)
+    print(f"""
+Bot token is: '{envar.BOT_TOKEN}'
+Api key is: '{envar.AI_KEY}'
+""")
 
     intents = discord.Intents.default()
     intents.message_content = True
+    intents.members = True
     bot = SayoryBot(intents=intents)
     bot.run(envar.BOT_TOKEN)
 
 class SayoryBot(discord.Client):
     async def on_ready(self) -> None:
-        print(f"Logged in as {self.user}")
+        self.ai = cohere.ClientV2(envar.AI_KEY)
+        self.model = "command-a-03-2025"
+        self.personality = "You're Sayori from doki doki lieterature club and someone says:"
+        self.answer_format = "You should answer often in a short message without \"\"!"
+
+        print(f"Logged in as: '{self.user}'")
 
     async def on_message(self, message: discord.Message) -> None:
-        print(f"> {message.author.name}: {message.content}")
+        print(f"Raw message from {message.author.name}: '{message.content}'")
 
-        if message.author.name == self.user.name:
+        if message.author == self.user:
             return # The bot should not respond to its own messages
 
         if self.user.mention in message.content:
-            await message.channel.send(f"Hello, {message.author.mention}! How can I help you?")
+            message_content = replace_id_with_displayname(message)
+            print(f"Processed message: '{message_content}'")
+
+            response = self.ai.chat(
+                model=self.model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": self.personality + ", " + self.answer_format
+                    },
+                    {
+                        "role": "user",
+                        "content": message_content
+                    }
+                ]
+            )
+
+            await message.channel.send(response.message.content[0].text)
+        elif "uwu" in message.content.lower():
+            response = self.ai.chat(
+                model=self.model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": self.personality + ", " + self.answer_format
+                    },
+                    {
+                        "role": "user",
+                        "content": "dont't talk or start a conversation just do something cute!"
+                    }
+                ]
+            )
+
+            await message.channel.send(response.message.content[0].text)
+
+def replace_id_with_displayname(message: discord.Message) -> str:
+    content = message.content
+    user_ids = get_ids(content)
+
+    for i in user_ids:
+        print(f"Id: {i}")
+        member = message.guild.get_member(int(i))
+        print(f"Member: {member}")
+        content = content.replace(f"<@{i}>", member.display_name)
+    
+    return content
+
+def get_ids(message: str) -> list[str]:
+    user_ids = re.findall(r"<@!?(\d+)>", message)
+    return user_ids
 
 if __name__ == "__main__":
     main()
